48cae136ed6c5280f6ebdb2c8a1552b9c935ecd0
Pavol Karlík <pavol.karlik@thermofisher.com>
2026-02-14 17:48:39 +0100

    Added ability to create github static page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/agentfortress/',
   plugins: [react()],
   resolve: {
     alias: {
